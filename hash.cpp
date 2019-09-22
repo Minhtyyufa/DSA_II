a6a2f7aaa8dd997ccf47a5efdde81e0d26aaae30
Minh-Thai <minhtyyufa@gmail.com>
2019-09-22 18:01:44 -0400

    move

--- hash.cpp
+++ hash.cpp
@@ -58,7 +58,6 @@ int hash_table::hash(const std::string &key)
     for(char c: key) {
         hash_val = hash_val * 37 + c;
     }
-
     return hash_val % capacity;
 }
 
@@ -66,7 +65,7 @@ unsigned int hash_table::get_prime(int size)
 {
     unsigned int primes[26] = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157,
                                98317, 196613, 393241, 786433, 1572869, 3145739, 6291469, 12582917, 25165843, 50331653,
-                               100663319, 201326611, 402653189, 805306457 ,1610612741};
+                               100663319, 201326611, 402653189, 805306457, 1610612741};
     for(unsigned int prime: primes)
     {
         if (prime > (unsigned int)size)
@@ -116,7 +115,6 @@ bool hash_table::rehash()
             std::cout<<"Error: "<< e.what() << std::endl;
             return false;
         }
-        std::vector<hash_item> test;
         capacity = new_cap;
         for(auto i : data)
         {
@@ -129,7 +127,7 @@ bool hash_table::rehash()
             }
         }
 
-        data = temp;
+        data = std::move(temp);
         return true;
     }
 }
